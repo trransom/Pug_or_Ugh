85699e8248bb2d9c6fceebf319889cb0b9c1e125
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-09-25 18:41:46 -0400

    Added models and serializers

--- backend/pugorugh/models.py
+++ backend/pugorugh/models.py
@@ -1,4 +1,64 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class Dog(models.Model):
+	GENDERS = (
+		('m', 'male'),
+		('f', 'female'),
+		('u', 'unknown'),
+	)
+	SIZES = (
+		('s', 'small'),
+		('m', 'medium'),
+		('l', 'large'),
+		('xl', 'extra large'),
+		('u', 'unknown'),
+	
+	)
+	name = models.CharField(max_length=200)
+	image_filename = models.CharField(max_length=300)
+	breed = models.CharField(max_length=200)
+	age = models.IntegerField(blank=True)
+	gender = models.CharField(max_length=50, choices=GENDERS)
+	size = models.CharField(max_length=50, choices=SIZES)
+	
+	def __str__(self):
+		return self.name
 
+class UserDog(models.Model):
+	STATUS = (
+		('l', 'liked'),
+		('d', 'disliked'),
+	)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+	status = models.CharField(max_length=10, choices=STATUS)
+	
+	def __str__(self):
+		return self.dog.name
+
+
+class UserPref(models.Model):
+	AGES = (
+		('b', 'baby'),
+		('y', 'young'),
+		('a', 'adult'),
+		('s', 'senior'),
+	)
+	GENDER = (
+		('m', 'male'),
+		('f', 'female'),
+	)
+	SIZE = (
+		('s', 'small'),
+		('m', 'medium'),
+		('l', 'large'),
+		('xl', 'extra large'),
+	)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	age = models.CharField(max_length=10, choices=AGES)
+	gender = models.CharField(max_length=10, choices=GENDER)
+	size = models.CharField(max_length=20, choices=SIZE)
+	
+	def __str__(self):
+		return self.user.username
--- backend/pugorugh/serializers.py
+++ backend/pugorugh/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
+from . import models
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -16,3 +18,27 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
+		
+		
+class DogSerializer(serializers.ModelSerializer):
+	class Meta:
+		fields = (
+			'name',
+			'image_filename',
+			'breed',
+			'age',
+			'gender',
+			'size'
+		)
+		model = models.Dog
+
+class UserPrefSerializer(serializers.ModelSerializer):
+	class Meta:
+		fields = (
+			'age',
+			'gender',
+			'size'
+		)
+		model = models.UserPref
+		
+
