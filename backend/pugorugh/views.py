d1e4e221826dada145b23d7dede65c7befb5843b
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-09-26 17:16:20 -0400

    Added imports to views.py

--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from . import serializers
+from . import models
 
 
 class UserRegisterView(CreateAPIView):
